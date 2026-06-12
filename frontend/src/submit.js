c37fca77d575b40d80c8f3942160552c7aee5da9
Ayush Sandeep Korde <ayush.korde_it@pvgcoenashik.org>
2026-06-12 16:37:58 +0530

    feat: connect frontend to Render backend

--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -13,7 +13,7 @@ export const SubmitButton = () => {
   const handleSubmit = async () => {
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:8000/pipelines/parse', {
+      const response = await fetch('https://vectorshift-assessment-hgbo.onrender.com/pipelines/parse', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nodes, edges }),
